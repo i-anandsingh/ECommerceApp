d540ea90239ab5018f3c73b5fc78252467eec07f
i-anandsingh <singh.anand974@gmail.com>
2024-04-09 00:42:59 +0530

    SellerRegistrationRepository Corrected

--- src/main/java/org/funapp/repository/SellerRegistrationRepository.java
+++ src/main/java/org/funapp/repository/SellerRegistrationRepository.java
@@ -2,6 +2,8 @@ package org.funapp.repository;
 
 import org.funapp.entity.SellerRegistrationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SellerRegistrationRepository extends JpaRepository<SellerRegistrationEntity, String> {
 }

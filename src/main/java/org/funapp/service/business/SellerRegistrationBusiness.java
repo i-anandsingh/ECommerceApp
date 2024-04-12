146772cad66f171816e41cfc078e9033f0cda8ae
i-anandsingh <singh.anand974@gmail.com>
2024-04-12 13:07:39 +0530

    Added IdGeneratorClient to code

--- src/main/java/org/funapp/service/business/SellerRegistrationBusiness.java
+++ src/main/java/org/funapp/service/business/SellerRegistrationBusiness.java
@@ -1,11 +1,11 @@
 package org.funapp.service.business;
 
+import generatorClient.generator.IdGenerator;
 import org.funapp.apimodels.mappers.SellerRegistrationDataMapper;
 import org.funapp.entity.SellerRegistrationEntity;
 import org.funapp.repository.SellerRegistrationRepository;
 import org.funapp.service.models.request.SellerRegistrationInputDTO;
 import org.funapp.service.models.response.SellerRegistrationOutputDTO;
-import org.funapp.utility.IdGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,14 +15,13 @@ import javax.validation.Valid;
 public class SellerRegistrationBusiness {
 
     private final SellerRegistrationRepository sellerRegistrationRepository;
-    private SellerRegistrationDataMapper sellerRegistrationDataMapper = SellerRegistrationDataMapper.INSTANCE;
+    private final SellerRegistrationDataMapper sellerRegistrationDataMapper = SellerRegistrationDataMapper.INSTANCE;
     private final IdGenerator idGenerator;
 
     @Autowired
-    public SellerRegistrationBusiness(SellerRegistrationRepository sellerRegistrationRepository,
-                                      IdGenerator idGenerator) {
-        this.idGenerator = idGenerator;
+    public SellerRegistrationBusiness(SellerRegistrationRepository sellerRegistrationRepository, IdGenerator idGenerator){
         this.sellerRegistrationRepository = sellerRegistrationRepository;
+        this.idGenerator = idGenerator;
     }
 
     public SellerRegistrationOutputDTO doProcess(@Valid SellerRegistrationInputDTO inputDTO){
